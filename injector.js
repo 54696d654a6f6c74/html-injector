63819fde35fbda96bd91460cbcdd339ae208a9fc
54696d654a6f6c74 <eniz.h12@gmail.com>
2021-02-04 00:09:54 +0200

    Implemented a better way of injecting tags

--- injector.js
+++ injector.js
@@ -1,3 +1,6 @@
+/**
+ * @deprecated Use bindHTML instead. This function will be removed in a future version!
+ */
 function injectHTML(injection, target)
 {
     if(Array.isArray(injection))
@@ -11,6 +14,9 @@ function injectHTML(injection, target)
     }
 }
 
+/**
+ * @deprecated Use bindHTML instead. This function will be removed in a future version!
+ */
 function injectHTMLTop(injection, target)
 {
     if(Array.isArray(injection))
@@ -24,6 +30,51 @@ function injectHTMLTop(injection, target)
     }
 }
 
+function createElement(tag)
+{
+    var ele = document.createElement(tag.tag);
+    
+    for(let i = 0; i < tag.atribs.length; i++)
+    {
+        var att = document.createAttribute(tag.atribs[i].name);
+        att.value = tag.atribs.value;
+        ele.setAttributeNode(att);
+    }
+
+    ele.innerHTML = tag.content;
+
+    return ele;
+}
+
+function bindHTML(injection, target) {
+    if(Array.isArray(injection))
+    {
+        var elems = [];
+        for(let i = 0; i < injection.length; i++)
+            elems.push(createElement(injection[i]));
+
+        if(Array.isArray(target))
+        {
+            if(target.length != injection.length)
+                throw new Error("The the amount of targets must match the amount of injections!");
+            for(let i = 0; i < target.length; i++)
+                document.getElementById(target[i]).appendChild(elems[i]);
+        }
+        else{
+            for(let i = 0; i < elems.length; i++)
+            document.getElementById(target).appendChild(elems[i]);
+        }
+    }
+
+    else
+    {
+        var elem = createElement(injection);
+
+        document.getElementById(target).appendChild(elem);
+    }
+}
+
 module.exports = { injectHTML: injectHTML,
-                   injectHTMLTop: injectHTMLTop
+                   injectHTMLTop: injectHTMLTop,
+                   bindHTML
 };
